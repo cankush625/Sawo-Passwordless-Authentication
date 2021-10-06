3f7b263bad05d0bec08eafca226d4760212eca0f
cankush625 <cankush625@gmail.com>
2021-10-06 22:41:18 +0530

    Initial commit
    Signed-off-by: cankush625 <cankush625@gmail.com>

---
+++ android/app/src/main/kotlin/com/example/sawo_passwordless_auth/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.sawo_passwordless_auth
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:sawo/sawo.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: 'Sawo Login Demo',
+    home: HomeScreen(),
+  ));
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Sawo login demo'),
+      ),
+      body: Center(child: SelectionButton()),
+    );
+  }
+}
+class SelectionButton extends StatefulWidget {
+  @override
+  _SelectionButtonState createState() => _SelectionButtonState();
+}
+
+
+class _SelectionButtonState  extends State<SelectionButton> {
+  Sawo sawo = Sawo(
+    apiKey: "YOUR_API_KEY",
+    secretKey: "YOUR_SECRET_KEY",
+  );
+
+  String user = "";
+
+  void payloadCallback(context, payload) {
+    if (payload == null || (payload is String && payload.length == 0)) {
+      payload = "Login Failed :(";
+    }
+    setState(() {
+      user = payload;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("UserData :- $user"),
+            ElevatedButton(
+              onPressed: () {
+                sawo.signIn(
+                  context: context,
+                  identifierType: 'email',
+                  callback: payloadCallback,
+                );
+              },
+              child: Text('Email Login'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                sawo.signIn(
+                  context: context,
+                  identifierType: 'phone_number_sms',
+                  callback: payloadCallback,
+                );
+              },
+              child: Text('Phone Login'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
